9f595b1abca7779dcdcb2448db18368b336282f4
Evan Edsa Azola <evanedsaazola@gmail.com>
2024-12-06 14:21:32 +0700

    Initial Commit to Github

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center', //adding files
+        alignItems: 'center'
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
